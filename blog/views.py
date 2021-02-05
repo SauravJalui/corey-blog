f0f53838c04bc149e293e68a9eecd01f5ea7663c
SauravJalui <saurav.jalui-codes@outlook.com>
2021-02-05 16:20:28 +0530

    completed part 4: Admins and part 5: Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Saurav Jalui',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'Feb 05, 2021',
-    },
-    {
-        'author' : 'Ram Yadav',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'Feb 06, 2021',
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
